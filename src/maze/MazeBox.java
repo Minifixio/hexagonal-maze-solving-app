46a974d18c3db6264789aab9e42eea6a895d2dd8
Emile Le Gallic <emile.legallic@telecom-paris.fr>
2022-11-03 18:45:57 +0100

    Lecture et affichage d'un labyrinthe par lecture d'un fichier txt

--- src/maze/MazeBox.java
+++ src/maze/MazeBox.java
@@ -2,19 +2,20 @@ package maze;
 
 import dijkstra.Vertex;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MazeBox implements Vertex {
-
     public int x;
     public int y;
-
     private Maze maze;
+    private boolean processed;
 
     public MazeBox(Maze maze, int x, int y) {
         this.x = x;
         this.y = y;
         this.maze = maze;
+        this.processed = false;
     }
 
     public void setDistance(int distance) {
@@ -26,10 +27,29 @@ public class MazeBox implements Vertex {
     }
 
     public boolean isProcessed() {
-        return false;
+        return this.processed;
     }
 
     public List<Vertex> getSuccessors() {
-        return null;
+        List<Vertex> successors = new ArrayList<>();
+        for (int i=-1; i<=1; i=i+2) {
+            if (
+                    this.x+i > 0
+                    && this.x+i < this.maze.width
+                    && !(this.maze.getBoxByCoords(this.x+i, this.y) instanceof WallMazeBox)
+                    && !(this.maze.getBoxByCoords(this.x+i, this.y).isProcessed())
+            ) {
+                successors.add(this.maze.getBoxByCoords(this.x+i, this.y));
+            }
+            if (
+                    this.y+i > 0
+                    && this.y+i < this.maze.length
+                    && !(this.maze.getBoxByCoords(this.x, this.y+i) instanceof WallMazeBox)
+                    && !(this.maze.getBoxByCoords(this.x, this.y+i).isProcessed())
+            ) {
+                successors.add(this.maze.getBoxByCoords(this.x, this.y+i));
+            }
+        }
+        return successors;
     }
 }
