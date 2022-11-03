bad71e742c5e662e72c1ed9eb5a75c4c9dc87443
Emile Le Gallic <emile.legallic@telecom-paris.fr>
2022-11-03 18:45:57 +0100

    Lecture et affichage d'un labyrinthe par lecture d'un fichier txt

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -7,10 +7,65 @@ import java.util.List;
 
 public class Maze implements Graph {
 
-    public MazeBox[][] maze;
+    public MazeBox[][] boxes;
+    public int width;
+    public int length;
 
-    public void Maze(int size) {
-        this.maze = new MazeBox[size][size];
+    public Maze(int width, int length) {
+        this.width = width;
+        this.length = length;
+        this.boxes = new MazeBox[width][length];
+    }
+
+    /**
+     * @param x coord x de la case
+     * @param y coord y de la case
+     * @param type le type de case selon la lettre correspondante ('E', 'W', 'A' ou 'D')
+     * @return true si la case est valide (i.e type est du type 'E', 'W', 'A' ou 'D'), false sinon
+     */
+    public boolean addBoxByCoords(int x, int y, char type) {
+        switch (type) {
+            case 'W':
+                this.boxes[x][y] = new WallMazeBox(this, x, y);
+                return true;
+            case 'E':
+                this.boxes[x][y] = new EmptyMazeBox(this, x, y);
+                return true;
+            case 'A':
+                this.boxes[x][y] = new ArrivalMazeBox(this, x, y);
+                return true;
+            case 'D':
+                this.boxes[x][y] = new DepartureMazeBox(this, x, y);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void printMaze() {
+        for (int i=0; i<this.length; i++) {
+            String line = "";
+            for (int j=0; j<this.width; j++) {
+                switch(this.getBoxByCoords(j,i).getClass().getSimpleName()) {
+                    case ("EmptyMazeBox"):
+                        line += "+";
+                        break;
+                    case ("WallMazeBox"):
+                        line += "#";
+                        break;
+                    case ("ArrivalMazeBox"):
+                        line += "X";
+                        break;
+                    case ("DepartureMazeBox"):
+                        line += "O";
+                        break;
+                }
+            }
+            System.out.println(line);
+        }
+    }
+    public Vertex getBoxByCoords(int x, int y) {
+        return this.boxes[x][y];
     }
 
     public List<Vertex> getAllVertexes() {
@@ -18,6 +73,10 @@ public class Maze implements Graph {
     }
 
     public List<Vertex> getAllVertexesSorted() {
+        return null;
+    }
+
+    public List<Vertex> getSuccessors(Vertex vertex) {
         return null;
     }
 
