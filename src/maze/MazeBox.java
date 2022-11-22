5bd28bc8e8ec14f32cd732a0854e1fb6b5823831
Emile Le Gallic <emile.legallic@telecom-paris.fr>
2022-11-22 20:27:50 +0100

    Correction de la distance dans une grille hexagonale

--- src/maze/MazeBox.java
+++ src/maze/MazeBox.java
@@ -54,29 +54,57 @@ public class MazeBox implements Vertex {
 
     public List<Vertex> getSuccessors() {
         List<Vertex> successors = new ArrayList<>();
-        for (int i=-1; i<1; i=i+1) {
-            for (int j=-1; j<2; j+=1) {
-                if (
-                        this.x+i >= 0
-                        && this.x+i < this.maze.width
-                        && this.y+j >= 0
-                        && this.y+j < this.maze.height
-                        && !(i==0 && j==0)
-                        && !(this.maze.getBoxByCoords(this.x+i, this.y) instanceof WallMazeBox)
-                ) {
-                    successors.add(this.maze.getBoxByCoords(this.x+i, this.y+j));
+
+        if (this.y % 2 == 0) {
+            for (int i=0; i<2; i++) {
+                for (int j=-1; j<2; j++) {
+                    if (
+                            this.x+i >= 0
+                            && this.x+i < this.maze.width
+                            && this.y+j >= 0
+                            && this.y+j < this.maze.height
+                            && !(i==0 && j==0)
+                            && !(this.maze.getBoxByCoords(this.x+i, this.y+j).getType() == 'W')
+                    ) {
+                        successors.add(this.maze.getBoxByCoords(this.x+i, this.y+j));
+                    }
                 }
             }
+            if (
+                    this.x-1 >= 0
+                    && this.x-1 < this.maze.width
+                    && this.y >= 0
+                    && this.y < this.maze.height
+                    && !(this.maze.getBoxByCoords(this.x-1, this.y).getType() == 'W')
+            ) {
+                successors.add(this.maze.getBoxByCoords(this.x-1, this.y));
+            }
+        } else {
+            for (int i=-1; i<1; i++) {
+                for (int j=-1; j<2; j++) {
+                    if (
+                            this.x+i >= 0
+                            && this.x+i < this.maze.width
+                            && this.y+j >= 0
+                            && this.y+j < this.maze.height
+                            && !(i==0 && j==0)
+                            && !(this.maze.getBoxByCoords(this.x+i, this.y+j).getType() == 'W')
+                    ) {
+                        successors.add(this.maze.getBoxByCoords(this.x+i, this.y+j));
+                    }
+                }
+            }
+            if (
+                    this.x+1 >= 0
+                    && this.x+1 < this.maze.width
+                    && this.y >= 0
+                    && this.y < this.maze.height
+                    && !(this.maze.getBoxByCoords(this.x+1, this.y).getType() == 'W')
+            ) {
+                successors.add(this.maze.getBoxByCoords(this.x+1, this.y));
+            }
         }
-        if (
-                this.x+1 >= 0
-                && this.x+1 < this.maze.width
-                && this.y >= 0
-                && this.y < this.maze.height
-                && !(this.maze.getBoxByCoords(this.x+1, this.y) instanceof WallMazeBox)
-        ) {
-            successors.add(this.maze.getBoxByCoords(this.x+1, this.y));
-        }
+
 
         return successors;
     }
